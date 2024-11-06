70f58aca2a1aa7ee313f74df54036033e5b4534b
emiperezdev <emiperez.dev@gmail.com>
2024-11-06 15:25:58 -0600

    add: implement express.

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import 'express-async-errors';
+// import { envs } from "./config/envs";
+// import { MongoDatabase } from "./data/init";
+import app from "./start/app";
+// import { emailJob } from './jobs/email.job';
+
+// (async () =>
+//   await MongoDatabase.connect({
+//     mongoUrl: envs.MONGO_URL,
+//     dbName: envs.MONTO_DB,
+//   }))();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`>>> Listening on port ${PORT}...`);
+  // emailJob();
+});
---
+++ src/start/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import morgan from 'morgan';
+// import { AppRoutes } from '../presentation/AppRoutes';
+// import error from '../middleware/error';
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+// app.use(AppRoutes.routes);
+
+// app.use(error);
+
+export default app; 
