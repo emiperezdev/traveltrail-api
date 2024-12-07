ff06cac449e926e69bea4bdfab1ffecf9ca8708e
emiperezdev <emiperez.dev@gmail.com>
2024-12-07 17:52:20 -0600

    add: implement cors in api

--- src/start/app.ts
+++ src/start/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
 import error from '../middleware/error';
+import cors from 'cors';
 import { AppRoutes } from '../presentation/AppRoutes';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(morgan('dev'));
